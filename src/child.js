3e2bbf23aaccc413b05afe2b331375b6242b5bce
Umair Younus <41501844+umairyounus1997@users.noreply.github.com>
2020-06-21 23:01:42 +0500

    Last Commiot

---
+++ src/child.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Child(props) {
+  return (
+    <div>
+      Hello child
+    </div>
+  );
+}
+
+export default Child;
---
+++ src/parent.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Child from 'child';
+
+function Parent(props) {
+  return (
+    <div>
+     Parent
+     <Child></Child>
+     </div>
+  );
+}
+
+export default Parent;
